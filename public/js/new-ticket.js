55bf459129b66223f17c23b5135f03cb38d23211
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-24 16:46:29 -0300

    socket - cliente

--- public/js/new-ticket.js
+++ public/js/new-ticket.js
@@ -1,2 +1,24 @@
+const newTicket = document.querySelector('#newTicket');
+const generateBtn = document.querySelector('button');
 
+const socket = io();
 
+socket.on('connect', () => {
+    console.log(`#### Conectado ####`);
+    generateBtn.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    console.log(`#### Desconectado ####`);
+    generateBtn.disabled = true;
+});
+
+socket.on('last-ticket', (ticket) => {
+    newTicket.textContent = `Ticket ${ticket}`;
+})
+
+generateBtn.addEventListener('click', () => {
+    socket.emit('next-ticket', null, (ticket) => {
+        newTicket.textContent = ticket;
+    });
+});
