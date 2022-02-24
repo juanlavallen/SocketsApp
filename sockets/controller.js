87a1701b2e670a3346cc263d9d75f7e9fce125ec
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-24 16:45:56 -0300

    socket controller - servidor

--- sockets/controller.js
+++ sockets/controller.js
@@ -2,7 +2,15 @@ const TicketControl = require('../models/TicketControl');
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
+    
+    socket.emit('last-ticket', ticketControl.last);
 
+    socket.on('next-ticket', (payload, callback) => {
+        const next = ticketControl.next();
+        callback(next);
+        // Notificando nuevo ticket 
+
+    });
 }
 
 module.exports = {
