c81b49e9631afcc02d2116bb1bdacdf1cb1f2a89
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-27 12:58:19 -0300

    pantalla publica - socket server

--- sockets/controller.js
+++ sockets/controller.js
@@ -2,7 +2,7 @@ const TicketControl = require('../models/TicketControl');
 const ticketControl = new TicketControl();
 
 const socketController = (socket) => {
-    
+
     socket.emit('last-ticket', ticketControl.last);
 
     socket.on('next-ticket', (payload, callback) => {
@@ -10,6 +10,28 @@ const socketController = (socket) => {
         callback(next);
         // Notificando nuevo ticket 
 
+    });
+
+    socket.on('attend-ticket', ({ desktop }, callback) => {
+        if (!desktop) {
+            return callback({
+                ok: false,
+                msg: 'El escritorio es obligatorio'
+            });
+        }
+
+        const ticket = ticketControl.attend(desktop);
+        if (!ticket) {
+            callback({
+                ok: false,
+                msg: 'No hay mas tickets pendientes'
+            });
+        } else {
+            callback({
+                ok: true,
+                ticket
+            });
+        }
     });
 }
 
