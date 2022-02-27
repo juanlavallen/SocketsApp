b5aca21dccf6b7256138f812768479f3adc93fc7
Juan Lavallen <juanlavallen02@gmail.com>
2022-02-27 12:36:08 -0300

    configuracion para comenzar

--- public/js/desktop.js
+++ public/js/desktop.js
@@ -1,2 +1,21 @@
+const renderDesktop = document.querySelector('h1');
+const btnAttend = document.querySelector('button');
 
+const socket = io();
 
+const searchParams = new URLSearchParams(window.location.search);
+if (!searchParams.has('desktop')) {
+    window.location = 'index.html';
+    throw new Error('El escritorio es obligatorio');
+}
+
+const desktop = searchParams.get('desktop');
+renderDesktop.innerText = desktop;
+
+socket.on('connect', () => {
+    btnAttend.disabled = false;
+});
+
+socket.on('disconnect', () => {
+    btnAttend.disabled = true;
+});
