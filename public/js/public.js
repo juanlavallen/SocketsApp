55dab3b535dba8842679a3fa8bc1c174335a2499
Juan Lavallen <juanlavallen02@gmail.com>
2022-03-01 17:05:24 -0300

    referencias y escuchando "last-four"

--- public/js/public.js
+++ public/js/public.js
@@ -1,1 +1,14 @@
+const ticket1 = document.querySelector('#ticket1');
+const ticket2 = document.querySelector('#ticket2');
+const ticket3 = document.querySelector('#ticket3');
+const ticket4 = document.querySelector('#ticket4');
+const desktop1 = document.querySelector('#desktop1');
+const desktop2 = document.querySelector('#desktop2');
+const desktop3 = document.querySelector('#desktop3');
+const desktop4 = document.querySelector('#desktop4');
 
+const socket = io();
+
+socket.on('last-four', (payload) => {
+
+});
